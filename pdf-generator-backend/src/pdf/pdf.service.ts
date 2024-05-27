1c62702798e3deb25c66ff9bec6287d6b4c57d45
Chaman Soni <55512349+chaman990@users.noreply.github.com>
2024-05-27 12:27:34 +0530

    Updates and fixes

--- pdf-generator-backend/src/pdf/pdf.service.ts
+++ pdf-generator-backend/src/pdf/pdf.service.ts
@@ -8,7 +8,7 @@ export class PdfService {
     const doc = new PDFDocument();
     const filePath = 'example.pdf';
     doc.pipe(fs.createWriteStream(filePath));
-    doc.fontSize(25).text(
+    doc.fontSize(10).text(
       `Hi Name is ${user?.name}, you can send me mail at ${user?.email}
           and if possible call me at ${user?.phone}, or send me a letter at ${user?.address}
         `,
